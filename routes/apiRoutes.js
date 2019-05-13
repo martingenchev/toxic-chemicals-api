7a609e7cf704993611df8f3cbe0bc1c12a7a523a
Martin.G <martin.g.wiredelta@gmail.com>
2019-05-13 19:53:53 +0200

    indentation algorithm solved. To be Refactored next step

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,13 +2,13 @@ const  router = require('express').Router();
 // remember that we are exporting a function from the modules warehouse
 // so put () after require
 WarehouseController = require('../Controllers/warehouse')();
-
+//api/Data
 router.get("/Data", function(req,res,next){
     WarehouseController.getData().then( (result,reject)=>{
         res.json(result)
     }).catch(next);
 });
-
+//localhost:3000/api/PutData
 router.post('/putData', function(req,res,next){
     WarehouseController.InsertData(req.body).then((result,reject)=>{
         res.json(result);
